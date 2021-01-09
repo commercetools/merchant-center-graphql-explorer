3c4e537f31b1bad9eec370ad8d5c48438036aea3
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-09 16:25:09 +0100

    refactor: simplify app setup (#78)

--- src/components/entry-point/entry-point.js
+++ src/components/entry-point/entry-point.js
@@ -1,9 +1,7 @@
-import { lazy, Component } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { lazy } from 'react';
 import {
   ApplicationShell,
   setupGlobalErrorListener,
-  RouteCatchAll,
 } from '@commercetools-frontend/application-shell';
 import { Sdk } from '@commercetools-frontend/sdk';
 import * as globalActions from '@commercetools-frontend/actions-global';
@@ -14,55 +12,29 @@ import loadMessages from '../../load-messages';
 // Splitting by route is usually recommended and you can potentially have a splitting
 // point for each route. More info at https://reactjs.org/docs/code-splitting.html
 const AsyncApplicationRoutes = lazy(() =>
-  import('../../routes' /* webpackChunkName: "starter-routes" */)
+  import('../../routes' /* webpackChunkName: "app-routes" */)
 );
-
-export const ApplicationStarter = () => (
-  <Switch>
-    {
-      /* For development, it's useful to redirect to the actual
-      application routes when you open the browser at http://localhost:3001 */
-      process.env.NODE_ENV === 'production' ? null : (
-        <Redirect
-          exact={true}
-          from="/:projectKey"
-          to="/:projectKey/graphql-explorer"
-        />
-      )
-    }
-    <Route
-      path="/:projectKey/graphql-explorer"
-      component={AsyncApplicationRoutes}
-    />
-    {/* Catch-all route */}
-    <RouteCatchAll />
-  </Switch>
-);
-ApplicationStarter.displayName = 'ApplicationStarter';
 
 // Ensure to setup the global error listener before any React component renders
 // in order to catch possible errors on rendering/mounting.
 setupGlobalErrorListener();
 
-class EntryPoint extends Component {
-  static displayName = 'EntryPoint';
-  render() {
-    return (
-      <ApplicationShell
-        environment={window.app}
-        onRegisterErrorListeners={({ dispatch }) => {
-          Sdk.Get.errorHandler = (error) =>
-            globalActions.handleActionError(error, 'sdk')(dispatch);
-        }}
-        applicationMessages={loadMessages}
-        DEV_ONLY__loadNavbarMenuConfig={() =>
-          import('../../../menu.json').then((data) => data.default || data)
-        }
-        featureFlags={FEATURE_FLAGS}
-        render={() => <ApplicationStarter />}
-      />
-    );
-  }
-}
+const EntryPoint = () => (
+  <ApplicationShell
+    environment={window.app}
+    onRegisterErrorListeners={({ dispatch }) => {
+      Sdk.Get.errorHandler = (error) =>
+        globalActions.handleActionError(error, 'sdk')(dispatch);
+    }}
+    applicationMessages={loadMessages}
+    DEV_ONLY__loadNavbarMenuConfig={() =>
+      import('../../../menu.json').then((data) => data.default || data)
+    }
+    featureFlags={FEATURE_FLAGS}
+  >
+    <AsyncApplicationRoutes />
+  </ApplicationShell>
+);
+EntryPoint.displayName = 'EntryPoint';
 
 export default EntryPoint;
--- src/components/entry-point/index.js
+++ src/components/entry-point/index.js
@@ -1,2 +1,1 @@
 export { default } from './entry-point';
-export { ApplicationStarter } from './entry-point';
