da3fffb7be88522bedc2f43ab1b8545adfafb092
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-09 15:49:35 +0100

    refactor: simplify app setup

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
-import PropTypes from 'prop-types';
-import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { Switch, Route, Redirect, Link, useRouteMatch } from 'react-router-dom';
 import Text from '@commercetools-uikit/text';
 import Spacings from '@commercetools-uikit/spacings';
 import Constraints from '@commercetools-uikit/constraints';
@@ -35,69 +34,66 @@ query ProjectExtension {
 }
 `;
 
-const ApplicationRoutes = ({ match }) => (
-  <Switch>
-    <Route path={`${match.path}/platform`}>
-      <Editor target="ctp" initialQuery={initialQueryCtp} />
-    </Route>
-    <Route path={`${match.path}/users`}>
-      <Editor target="mc" initialQuery={initialQueryMc} />
-    </Route>
-    <Route path={`${match.path}/settings`}>
-      <Editor target="settings" initialQuery={initialQuerySettings} />
-    </Route>
-    <Redirect exact={true} from={`${match.path}/*`} to={match.url} />
-    <Route>
-      <Spacings.Inset>
-        <Constraints.Horizontal constraint="l">
-          <Spacings.Stack scale="m">
-            <Text.Headline as="h1">GraphQL Explorer</Text.Headline>
-            <Text.Body>
-              Explore some of the GraphQL APIs of the commercetools platform
-              using the GraphiQL IDE.
-            </Text.Body>
-          </Spacings.Stack>
-          <Spacings.Stack scale="s">
-            <ul>
-              <li>
-                <FlatButton
-                  as={Link}
-                  to={`${match.url}/platform`}
-                  label="Explore the commercetools platform GraphQL API"
-                />
-              </li>
-              <li>
-                <FlatButton
-                  as={Link}
-                  to={`${match.url}/users`}
-                  label="Explore the Merchant Center Users GraphQL API"
-                />
-              </li>
-              <li>
-                <FlatButton
-                  as={Link}
-                  to={`${match.url}/settings`}
-                  label="Explore the Merchant Center Settings GraphQL API"
-                />
-              </li>
-            </ul>
-            <ContentNotification type="warning">
-              Certain queries or mutations may be rejected given the lack of
-              OAuth Scopes associated to the permissions granted to the user for
-              this project.
-            </ContentNotification>
-          </Spacings.Stack>
-        </Constraints.Horizontal>
-      </Spacings.Inset>
-    </Route>
-  </Switch>
-);
-ApplicationRoutes.displayName = 'ApplicationRoutes';
-ApplicationRoutes.propTypes = {
-  match: PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  }).isRequired,
+const ApplicationRoutes = () => {
+  const match = useRouteMatch();
+  return (
+    <Switch>
+      <Route path={`${match.path}/platform`}>
+        <Editor target="ctp" initialQuery={initialQueryCtp} />
+      </Route>
+      <Route path={`${match.path}/users`}>
+        <Editor target="mc" initialQuery={initialQueryMc} />
+      </Route>
+      <Route path={`${match.path}/settings`}>
+        <Editor target="settings" initialQuery={initialQuerySettings} />
+      </Route>
+      <Redirect exact={true} from={`${match.path}/*`} to={match.url} />
+      <Route>
+        <Spacings.Inset>
+          <Constraints.Horizontal constraint="l">
+            <Spacings.Stack scale="m">
+              <Text.Headline as="h1">GraphQL Explorer</Text.Headline>
+              <Text.Body>
+                Explore some of the GraphQL APIs of the commercetools platform
+                using the GraphiQL IDE.
+              </Text.Body>
+            </Spacings.Stack>
+            <Spacings.Stack scale="s">
+              <ul>
+                <li>
+                  <FlatButton
+                    as={Link}
+                    to={`${match.url}/platform`}
+                    label="Explore the commercetools platform GraphQL API"
+                  />
+                </li>
+                <li>
+                  <FlatButton
+                    as={Link}
+                    to={`${match.url}/users`}
+                    label="Explore the Merchant Center Users GraphQL API"
+                  />
+                </li>
+                <li>
+                  <FlatButton
+                    as={Link}
+                    to={`${match.url}/settings`}
+                    label="Explore the Merchant Center Settings GraphQL API"
+                  />
+                </li>
+              </ul>
+              <ContentNotification type="warning">
+                Certain queries or mutations may be rejected given the lack of
+                OAuth Scopes associated to the permissions granted to the user
+                for this project.
+              </ContentNotification>
+            </Spacings.Stack>
+          </Constraints.Horizontal>
+        </Spacings.Inset>
+      </Route>
+    </Switch>
+  );
 };
+ApplicationRoutes.displayName = 'ApplicationRoutes';
 
 export default ApplicationRoutes;
