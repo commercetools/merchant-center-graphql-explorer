6495f153686a8df1a6349cb74124a95dab93a5f6
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-29 21:35:32 +0100

    refactor: remove unnecessary dependencies and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,5 @@
 process.env.ENABLE_NEW_JSX_TRANSFORM = 'true';
 
 module.exports = {
-  extends: [
-    '@commercetools-frontend/eslint-config-mc-app',
-    'plugin:testing-library/react',
-  ],
-  plugins: ['react-hooks'],
-  rules: {
-    'max-classes-per-file': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jest/expect-expect': 'off',
-    'testing-library/prefer-presence-queries': 'error',
-    'testing-library/await-async-query': 'error',
-  },
-  settings: { react: { version: '^16.14' } },
-  overrides: [
-    {
-      files: ['*.spec.js'],
-      rules: { 'prefer-promise-reject-errors': 0, 'no-console': 0 },
-    },
-  ],
+  extends: ['@commercetools-frontend/eslint-config-mc-app'],
 };
