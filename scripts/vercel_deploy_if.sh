bb2c41cec0dac9fa342618380ec1c07c7ce888e4
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-24 11:42:02 +0100

    fix: check also for master

--- scripts/vercel_deploy_if.sh
+++ scripts/vercel_deploy_if.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# NOTE: somehow Vercel defaults to `master` when checking out the code,
+# even though it's supposed to be `main`.
+# https://github.com/vercel/vercel/discussions/5171
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Branch: $branch_name"
 
-if [[ "$branch_name" == "main"  ]]; then
+if [[ "$branch_name" == "main" || "$branch_name" == "master" ]]; then
   # Proceed with the build
   exit 1;
 
