61a286b76a61456694ca1f911d0f4b5b15758d55
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-24 12:20:55 +0100

    fix: use VERCEL_GIT_COMMIT_REF

--- scripts/vercel_deploy_if.sh
+++ scripts/vercel_deploy_if.sh
@@ -3,18 +3,7 @@
 # NOTE: somehow Vercel defaults to `master` when checking out the code,
 # even though it's supposed to be `main`.
 # https://github.com/vercel/vercel/discussions/5171
-branch_name=$(git rev-parse --abbrev-ref HEAD)
-
-echo "git branch –show-current"
-git branch –show-current
-echo "git rev-parse –abbrev-ref HEAD"
-git rev-parse –abbrev-ref HEAD
-echo "git symbolic-ref –short HEAD"
-git symbolic-ref –short HEAD
-echo "git name-rev –name-only HEAD"
-git name-rev –name-only HEAD
-echo "Branch: $branch_name"
-exit 0
+branch_name="$VERCEL_GIT_COMMIT_REF"
 
 if [[ "$branch_name" == "main" || "$branch_name" == "master" ]]; then
   # Proceed with the build
