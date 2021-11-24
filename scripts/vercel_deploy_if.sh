43fec9caf67e4f58ddeab618c39cd3e240a3a5ca
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-24 12:17:57 +0100

    chore: more debug logs

--- scripts/vercel_deploy_if.sh
+++ scripts/vercel_deploy_if.sh
@@ -5,8 +5,14 @@
 # https://github.com/vercel/vercel/discussions/5171
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
-env | grep -e "VERCEL_"
-
+echo "git branch –show-current"
+git branch –show-current
+echo "git rev-parse –abbrev-ref HEAD"
+git rev-parse –abbrev-ref HEAD
+echo "git symbolic-ref –short HEAD"
+git symbolic-ref –short HEAD
+echo "git name-rev –name-only HEAD"
+git name-rev –name-only HEAD
 echo "Branch: $branch_name"
 exit 0
 
