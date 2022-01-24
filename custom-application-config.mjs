e044fe40ce580950e7322b19a8c7a046d374027f
Nicola Molinari <nicola.molinari@commercetools.com>
2022-01-24 13:24:18 +0100

    chore: migrate to appkit v21

---
+++ custom-application-config.mjs
@@ -0,0 +1,71 @@
+import { entryPointUriPath, PERMISSIONS } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptions}
+ */
+const config = {
+  name: 'GraphQL Explorer',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: '${env:CTP_INITIAL_PROJECT_KEY}',
+    },
+    production: {
+      applicationId: '${env:APPLICATION_ID}',
+      url: 'https://merchant-center-graphql-explorer-commercetools-playground.vercel.app',
+    },
+  },
+  oAuthScopes: {
+    view: [
+      'view_api_clients',
+      'view_cart_discounts',
+      'view_customer_groups',
+      'view_customers',
+      'view_discount_codes',
+      'view_import_containers',
+      'view_orders',
+      'view_payments',
+      'view_products',
+      'view_project_settings',
+      'view_shipping_methods',
+      'view_states',
+      'view_stores',
+      'view_tax_categories',
+      'view_types',
+    ],
+    manage: [],
+  },
+  headers: {
+    csp: {
+      'connect-src': ['${env:HOST_GCP_STAGING}'],
+    },
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    permissions: [PERMISSIONS.View],
+    defaultLabel: 'Graphql Explorer',
+    labelAllLocales: [],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'platform',
+      permissions: [PERMISSIONS.View],
+      defaultLabel: 'commercetools platform',
+      labelAllLocales: [],
+    },
+    {
+      uriPath: 'users',
+      permissions: [PERMISSIONS.View],
+      defaultLabel: 'Users',
+      labelAllLocales: [],
+    },
+    {
+      uriPath: 'settings',
+      permissions: [PERMISSIONS.View],
+      defaultLabel: 'Settings',
+      labelAllLocales: [],
+    },
+  ],
+};
+module.exports = config;
