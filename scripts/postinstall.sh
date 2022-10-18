f1508f1786a6ee7c5b46ecca9d0d597dbcf14ced
Nicola Molinari <nicola.molinari@commercetools.com>
2022-10-18 08:51:30 +0200

    refactor: upgrade graphiql to v2 (#164)
    * refactor: upgrade graphiql to v2
    
    * chore: explicitly drop corrupted linked package
    
    * fix(cypress): selector
    
    * ci: add preview deployment
    
    * ci: fix branch name
    
    * ci: add missing pull_request trigger
    
    * ci: fix variable

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo "Removing corrupted packages"
+
+# Relates to https://github.com/reach/reach-ui/issues/921
+rm -rf node_modules/graphiql/node_modules/@graphiql
+
 echo "Preparing development setup."
 
 yarn husky install
