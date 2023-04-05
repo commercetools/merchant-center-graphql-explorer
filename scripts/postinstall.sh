c75604eabcc600b10f9edbc77928bc88abc2aba4
Nicola Molinari <nicola.molinari@commercetools.com>
2023-04-05 17:38:09 +0200

    chore: migrate to pnpm (#180)

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -5,9 +5,9 @@ set -e
 echo "Removing corrupted packages"
 
 # Relates to https://github.com/reach/reach-ui/issues/921
-rm -rf node_modules/graphiql/node_modules/@graphiql
+# rm -rf node_modules/graphiql/node_modules/@graphiql
 
 echo "Preparing development setup."
 
-yarn husky install
-yarn manypkg check
+pnpm husky install
+pnpm manypkg check
