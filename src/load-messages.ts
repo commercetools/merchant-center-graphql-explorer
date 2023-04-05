05c98a5d4d78eec6dfe60fc81fba24b8032f4354
Nicola Molinari <nicola.molinari@commercetools.com>
2023-04-05 17:56:22 +0200

    chore: update deps

--- src/load-messages.ts
+++ src/load-messages.ts
@@ -1,34 +1,37 @@
-const loadMessages = (lang: string): Promise<Record<string, string>> => {
-  let loadAppI18nPromise;
-  switch (lang) {
-    case 'de':
-      loadAppI18nPromise = import(
-        './i18n/compiled-data/de.json' /* webpackChunkName: "app-i18n-de" */
-      );
-      break;
+import {
+  parseChunkImport,
+  type TMessageTranslations,
+  TI18NImportData,
+} from '@commercetools-frontend/i18n';
+
+const getChunkImport = (locale: string): Promise<TI18NImportData> => {
+  switch (locale) {
     case 'es':
-      loadAppI18nPromise = import(
+      return import(
         './i18n/compiled-data/es.json' /* webpackChunkName: "app-i18n-es" */
       );
-      break;
+    case 'de':
+      return import(
+        './i18n/compiled-data/de.json' /* webpackChunkName: "app-i18n-de" */
+      );
     default:
-      loadAppI18nPromise = import(
-        './i18n/compiled-data/en.json' /* webpackChunkName: "app-i18n-en" */
+      return import(
+        /* webpackChunkName: "app-i18n-en" */
+        './i18n/compiled-data/en.json'
       );
   }
-
-  return loadAppI18nPromise.then(
-    (result) => result.default,
-    (error) => {
-      // eslint-disable-next-line no-console
-      console.warn(
-        `Something went wrong while loading the app messages for ${lang}`,
-        error
-      );
-
-      return {};
-    }
-  );
+};
+const loadMessages = async (locale: string): Promise<TMessageTranslations> => {
+  try {
+    const chunkImport = await getChunkImport(locale);
+    return parseChunkImport(chunkImport);
+  } catch (error) {
+    console.warn(
+      `Something went wrong while loading the app messages for ${locale}`,
+      error
+    );
+    return {};
+  }
 };
 
 export default loadMessages;
