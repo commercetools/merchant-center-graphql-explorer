f76cd5b70b0e3aacc349536963a3e25e624ff3ee
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-02-24 13:35:20 +0100

    fix: allow uri path to be configured

--- merchant-center-graphql-explorer/src/constants.ts
+++ merchant-center-graphql-explorer/src/constants.ts
@@ -1,6 +1,13 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'graphql-explorer-commercetools';
+declare const window: Window &
+  typeof globalThis & {
+    app: { entryPointUriPath: string };
+  };
 
+export const entryPointUriPath =
+  typeof window === 'undefined'
+    ? process.env.ENTRY_POINT_URI_PATH || 'graphql-explorer-commercetools'
+    : window.app.entryPointUriPath;
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
