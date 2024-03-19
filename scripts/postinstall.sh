44af6e5fd2d73c5f8a0e96a4e7ed605866db86a0
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-03-19 09:17:24 +0100

    chore: migrate to yarn

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo "Removing corrupted packages"
-
-# Relates to https://github.com/reach/reach-ui/issues/921
-# rm -rf node_modules/graphiql/node_modules/@graphiql
-
 echo "Preparing development setup."
 
-pnpm husky install
-pnpm manypkg check
+yarn manypkg check
